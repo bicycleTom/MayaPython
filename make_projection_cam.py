7babb1df8f855e2feba1f51821faba5358be9c82
Tom <bicycle.tom@gmail.com>
2015-04-08 18:34:14 -0400

    Update make_projection_cam.py

--- make_projection_cam.py
+++ make_projection_cam.py
@@ -4,8 +4,9 @@ frame = str(frame)
 selected = pm.ls(selection=True)
 
 def cam_projection(selected):
-    if not n in cam_projection:
+    if not selected:
         pm.informBox(title="ERROR", message="Please Select Shot/Render Camera", ok="OK")
+        return None
     else:
         pm.duplicate(ic=False, name="proj_cam_fr" + frame)
         return None
