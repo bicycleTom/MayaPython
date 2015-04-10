8a4d0cec9bfb22420cf885e4beca6d46dfbdbd88
Tom <bicycle.tom@gmail.com>
2015-04-10 01:09:00 -0400

    Update make_projection_cam.py
    adding a ton of attribute connections for creating the shading network

--- make_projection_cam.py
+++ make_projection_cam.py
@@ -11,7 +11,38 @@ def cam_projection(selected):
         pm.duplicate(ic=False, name="proj_cam_fr" + frame)
         return None
         
-def projection_shader():
-    pm.shadingNode("surfaceShader", asShader = True, n = 'projection_fr' + frame)
+#def projection_shader():
+#    pm.shadingNode("surfaceShader", asShader = True, n = 'projection_fr' + frame)
         
 cam_projection(selected)
+
+projection_shader = pm.shadingNode("surfaceShader", asShader = True, n = 'projectionShader_fr' + frame)
+filename = pm.shadingNode("file", asTexture = True)
+twoDTexture = pm.shadingNode("place2dTexture", asUtility = True)
+projection_utility = pm.shadingNode("projection", asUtility = True)
+
+pm.setAttr(filename.filterType, 0)
+pm.setAttr(projection_utility.projType, 8)
+pm.setAttr(,projection_utility.linkedCamera)
+
+pm.connectAttr(projection_utility.outColor, projection_shader.outColor, f = True)
+pm.connectAttr(filename.outColor, projection_utility.image, f = True)
+
+pm.connectAttr(twoDTexture.coverage, filename.coverage, f = True)
+pm.connectAttr(twoDTexture.translateFrame, filename.translateFrame, f = True)
+pm.connectAttr(twoDTexture.rotateFrame, filename.rotateFrame, f = True)
+pm.connectAttr(twoDTexture.mirrorU, filename.mirrorU, f = True)
+pm.connectAttr(twoDTexture.mirrorV, filename.mirrorV, f = True)
+pm.connectAttr(twoDTexture.stagger, filename.stagger, f = True)
+pm.connectAttr(twoDTexture.wrapU, filename.wrapU, f = True)
+pm.connectAttr(twoDTexture.wrapV, filename.wrapV, f = True)
+pm.connectAttr(twoDTexture.repeatUV, filename.repeatUV, f = True)
+pm.connectAttr(twoDTexture.offset, filename.offset, f = True)
+pm.connectAttr(twoDTexture.rotateUV, filename.rotateUV, f = True)
+pm.connectAttr(twoDTexture.noiseUV, filename.noiseUV, f = True)
+pm.connectAttr(twoDTexture.vertexUvOne, filename.vertexUvOne, f = True)
+pm.connectAttr(twoDTexture.vertexUvTwo, filename.vertexUvTwo, f = True)
+pm.connectAttr(twoDTexture.vertexUvThree, filename.vertexUvThree, f = True)
+pm.connectAttr(twoDTexture.vertexCameraOne, filename.vertexCameraOne, f = True)
+pm.connectAttr(twoDTexture.outUV, filename.uv, f = True)
+pm.connectAttr(twoDTexture.outUvFilterSize, filename.uvFilterSize, f= True)
