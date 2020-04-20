16d02ebe767de9600bfd36ccf74fa4b96d13bcb8
Tom <bicycle.tom@gmail.com>
2020-04-20 12:43:50 +1000

    adding render settings default aovs

---
+++ MtoA_render_settings.py
@@ -0,0 +1,28 @@
+import maya.cmds as cmds
+import mtoa.aovs as aovs
+
+def main():
+
+	BUILTIN_AOVS = ['P', 'Z', 'N', 'opacity', 'motionvector', 'Pref', 'raycount', 'cputime', 'ID', 'RGBA', 'direct', 'indirect', 'emission', 'background', 'diffuse', 'specular',
+ 'transmission', 'sss', 'volume', 'albedo', 'diffuse_direct', 'diffuse_indirect', 'diffuse_albedo', 'specular_direct', 'specular_indirect', 'specular_albedo', 'coat',
+ 'coat_direct', 'coat_indirect', 'coat_albedo', 'sheen', 'sheen_direct', 'sheen_indirect', 'sheen_albedo', 'transmission_direct', 'transmission_indirect',
+ 'transmission_albedo', 'sss_direct', 'sss_indirect', 'sss_albedo', 'volume_direct', 'volume_indirect', 'volume_albedo', 'volume_opacity', 'volume_Z',
+ 'shadow_matte', 'AA_inv_density']
+
+	LIGHTING_AOVS = ['RGBA', 'direct', 'indirect', 'emission', 'diffuse', 'specular', 'transmission', 'sss', 'volume', 'diffuse_direct', 'diffuse_indirect', 'diffuse_albedo',
+ 'specular_direct', 'specular_indirect', 'specular_albedo', 'coat', 'coat_direct', 'coat_indirect', 'coat_albedo', 'transmission_direct', 'transmission_indirect',
+ 'transmission_albedo', 'sss_direct', 'sss_indirect', 'sss_albedo', 'volume_direct', 'volume_indirect', 'volume_albedo', 'shadow_matte', 'sheen', 'sheen_direct',
+ 'sheen_indirect', 'sheen_albedo']
+
+ 	PRODUCTION_AOVS = ['RGBA', 'direct', 'indirect']
+
+ 	EXISTING_AOVS = []
+ 	for aov in aovs.getAOVNodes(names=False):
+ 		EXISTING_AOVS.append(str(aov))
+ 	EXISTING_AOVS = [aov.replace('aiAOV_', '') for aov in EXISTING_AOVS]
+
+ 	NEW_AOVS = list(set(PRODUCTION_AOVS) - set(EXISTING_AOVS))
+ 	#print NEW_AOVS
+
+ 	for item in NEW_AOVS:
+ 		addAOV = aovs.AOVInterface().addAOV(item)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MayaPython
 snippets of tools speeding up repetitive tasks
+
+SPtoMtoA:
+Take Substance Painter exported textures and rebuild complete Arnold 6 shader tree in Maya
